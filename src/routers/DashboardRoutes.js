c54512995cd06c2c19361e3875465ae35ed1ed5e
Javier Lucero <javier.lucero@iess.gob.ec>
2021-12-30 16:21:06 -0500

    Creado el proyecto inicial

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,19 @@
+import { MarvelScreen } from "../components/marvel/MarvelScreen"
+import { Navbar } from "../components/ui/Navbar"
+import { Route, Routes } from "react-router-dom";
+import { DcScreen } from '../components/dc/DcScreen';
+import { SearchScreen } from '../components/search/SearchScreen';
+
+export const DashboardRoutes = () => {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="marvel" element={<MarvelScreen />} />
+                <Route path="dc" element={<DcScreen />} />
+                <Route path="search" element={<SearchScreen />} />
+                <Route path="/" element={<MarvelScreen />} />
+            </Routes>
+        </>
+    )
+}
